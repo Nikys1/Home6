8a45e98c4f3e38568c8f9c4db4b8439bc1a95ba6
Lesha <nikys19993@gmail.com>
2022-06-12 16:45:58 +0300

    12.06

--- Peerson.java
+++ Peerson.java
@@ -10,8 +10,31 @@ public class Peerson {
             this.secondName = secondName;
         }
 
-        public void add
+//     Зачисление денег на карту
 
+        public void addMoneyToCard(String number, int money){
+            for(int i = 0; i < creditCards.length; i++){
+                if (creditCards[i].number == number){
+                    creditCards[i].money += money;
+                    System.out.println(
+                            "На карту **** **** **** ****" + number.split(" ")[3] + " зачисленно. Сумма : " + money
+                    );
+                    System.out.println("Остаток на карте " + creditCards[i].money);
+                }
+            }
+        }
+//    Cписание денег
+    public void payMoneyToCard (String number, int money){
+            for(int i = 0; i < creditCards.length; i++){
+                if(creditCards[i].number == number){
+                    creditCards[i].money -= money;
+                    System.out.println(
+                            "С карты **** **** **** ****" + number.split(" ")[3] + " произошло списание. Остаток на счёте  " + money
+                    );
+                    System.out.println("Отаток на карте " + creditCards[i].money);
+                }
+            }
+    }
 
 
     }
