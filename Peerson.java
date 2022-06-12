db19a01bef6d676abb24929745ae27b0deec111a
Lesha <nikys19993@gmail.com>
2022-06-12 15:19:31 +0300

    1

---
+++ Peerson.java
@@ -0,0 +1,16 @@
+public class Peerson {
+
+        String firstName;
+        String secondName;
+
+
+        public Person(String firstName, String secondName){
+            this.firstName = firstName;
+            this.secondName = secondName;
+        }
+
+
+
+    }
+
+}
